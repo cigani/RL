f75a60abec94572b39b1c1c92497de7c749ee13e
Michael Jaquier <shibasu@gmail.com>
2017-01-02 21:47:25 +0100

    asdf

--- starter.py
+++ starter.py
@@ -44,17 +44,19 @@ class DummyAgent:
                                                              retstep=True)
         self.centers = [_x_space_, _x_d_space_]
 
-        #self.Q = np.zeros(3)
-        
+        # self.Q = np.zeros(3)
+
         # Trace Memory
         self.e = np.zeros((neurons, neurons, 3))
         self.weights = 0.01 * np.random.rand((neurons, neurons, 3)) + 0.1
 
         # self.activity = {"Right": 0, "Left": 1, "Neutral": 2}
-        self.activity = {"1": 0, "-1": 1, "0": 2}
+        self.action_index_ = {"1": 0, "-1": 1, "0": 2}
+        self.last_action = None
         self.action = None
+        self.old_state = None
+        self.state = [self.mountain_car.x, self.mountain_car.x_d]
 
-        
     def visualize_trial(self, n_steps=200):
         """Do a trial without learning, with display.
 
@@ -90,86 +92,76 @@ class DummyAgent:
                 print("\rreward obtained at t = ", self.mountain_car.t)
                 break
 
-            
     def learn(self):
         # This is your job!
         pass
 
-    
-    def input_layer_activity(self, centers, state_x, state_x_d):
-        rj = np.exp(((centers[0] - state_x) ** 2) /
+    def input_layer_activity(self, centers):
+        rj = np.exp(((centers[0] - self.state[0]) ** 2) /
                     self.x_centers_distance **
                     2 -
-                    ((centers[1] - state_x_d) ** 2) /
+                    ((centers[1] - self.state[1]) ** 2) /
                     self.phi_centers_distance
                     ** 2)
         return rj
 
-    
-    def output_layer_activity(self, action, state):
+    def output_layer_activity(self, action):
         weights = self.weights
-        action_index = self.activity["{}".format(action)]
+        action_index = self.action_index_["{}".format(action)]
         q_weights = 0.0
         for n in np.arange(self.neuron_count):
             q_weights += weights[n][action_index] * self.input_layer_activity(
-                self.centers[n], state[0], state[1])
+                self.centers[n])
         return q_weights
 
-    
     def td_error(self):
         reward = self.mountain_car.R
-        new_state = [self.x, self.x_d]
-        old_state = [self.old_x, self.old_x_d]
         self.dirac = reward - (
-            self.output_layer_activity(self.last_action, old_state) -
+            self.output_layer_activity(self.last_action) -
             self.gamma_ * self.output_layer_activity(
-                self.action, new_state))
-
+                self.action))
 
     def update_eligibility(self):
         """
         Eligibility updates using the SARSA protocol.
         """
-        self.e = self.lambda_ * self.gamma_ * self.e
-        self.e[:,:,self.last_action] += 1
+        action = self.action_index_["{}".format(self.last_action)]
+        self.e *= self.lambda_ * self.gamma_
+        self.e[:, :, action] += 1
 
-        
     def update_weights(self):
         """
         Weight updates using the SARSA protocol.
         """
-        td_error()
-        self.weights += self.dirac * self.eta * self.e
-        
-        
+        self.td_error()
+        self.weights += self.dirac * self.eta_ * self.e
+
     def action_choice(self):
         """
         Choose the next action based on the current Q-values.
         Determined by soft-max rule.
         Cumulative probabilities for quick implementation.
         """
-        c_prob_left = soft_max_rule(0);
-        c_prob_right = c_prob_left + soft_max_rule(1);
+        c_prob_left = self.soft_max_rule(0)
+        c_prob_right = c_prob_left + self.soft_max_rule(1)
 
-        test_value = np.random.rand();
-        
+        test_value = np.random.rand()
+
         if test_value < c_prob_left:
-            update_state(-1)
+            self.update_state(-1)
         elif test_value < c_prob_right:
-            update_state(1)
+            self.update_state(1)
         else:
-            update_state(0)
+            self.update_state(0)
 
-            
     def soft_max_rule(self, action):
         """
         Soft-max algorithm
         """
-        probability = (math.exp(output_layer_activity(action, self.state)
-                               /self.exploration_temperature)
-                       /all_actions())
+        probability = (np.exp(self.output_layer_activity(action)
+                              / self.initial_temperature_)
+                       / self.all_actions())
         return probability
-
 
     def all_actions(self):
         """
@@ -177,19 +169,20 @@ class DummyAgent:
         """
         total_Q = 0.0
         for action in np.arange(2):
-            total_Q += math.exp(output_layer_activity(action, self.state))
+            total_Q += np.exp(self.output_layer_activity(action))
         return total_Q
 
-    
     def update_state(self, command):
         self.last_action = self.action
         self.action = command
         self.old_x = self.mountain_car.x
         self.old_x_d = self.mountain_car.x_d
+        self.old_state = [self.old_x, self.old_x_d]
         self.mountain_car.apply_force(self.action)
         self.x = self.mountain_car.x
         self.x_d = self.mountain_car.x_d
-        update_eligibility()
+        self.state = [self.x, self.x_d]
+        self.update_eligibility()
 
 
 if __name__ == "__main__":
