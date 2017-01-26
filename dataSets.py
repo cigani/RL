9205cef79b8adfd4e88891097612b420fd8a1212
alorkowski <alexander.lorkowski@epfl.ch>
2017-01-26 19:32:55 +0100

    Added modified dataSets.py
    	modified:   dataSets.py

--- dataSets.py
+++ dataSets.py
@@ -2,15 +2,18 @@ import h5py
 import numpy as np
 
 
-def generate_data_save(h5data, _, time_to_reward_data, weights_data):
+def generate_data_save(h5data, _, time_to_reward_data, steps_to_reward_data, weights_data):
     h5data['time_to_reward'].create_dataset(
         "time_to_reward_{}".format(_), data=time_to_reward_data, compression="gzip")
+    h5data['steps_to_reward'].create_dataset(
+        "steps_to_reward_{}".format(_), data=steps_to_reward_data, compression="gzip")
     h5data['weights'].create_dataset(
         "weights_{}".format(_), data=weights_data, compression="gzip")
 
 def generate_data_sets(filename, centers):
     h5data = h5py.File(filename, 'w')
     h5data.create_group('time_to_reward')
+    h5data.create_group('steps_to_reward')
     h5data.create_group('neurons')
     h5data.create_group('weights')
 
